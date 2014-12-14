199f3c1d451d0e30a800f6564cb4bcbaa79ea9b4
chaneetsk <chaneetsk@hotmail.com>
2014-12-15 01:52:47 +1100

    yet another commit

--- _site/assets/js/functions.js
+++ _site/assets/js/functions.js
@@ -1,5 +1,18 @@
 $( document ).ready(function() {
 
-  //
+ 	smoothScroll(1000);
 
 });
+
+function smoothScroll(duration) {
+	$('a[href^="#"]').on('click', function(e){
+		var target = $($(this).attr('href'));
+		
+		if (target.length>0) {
+			e.preventDefault();
+			$('html, body').animate({
+				scrollTop: target.offset().top
+			},duration);
+		}
+	});
+}
